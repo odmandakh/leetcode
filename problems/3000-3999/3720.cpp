10a490babbea82ff02eec8a08abbf40f3d24a3e0
achess <coding.achess@gmail.com>
2026-08-27 11:36:44 +0800

    daily: aug 27

---
+++ problems/3000-3999/3720.cpp
@@ -0,0 +1,94 @@
+#include <optional>
+#include <vector>
+
+#include "runner.h"
+
+using namespace std;
+
+class Solution {
+ public:
+  string a = "";
+  vector<int> alph;
+  Solution() : alph(26, 0) {}
+
+  optional<int> backtrace(string s, string target, int index) {
+    for (int i = index; i >= 0; --i) {
+      bool flag = false;
+      for (char c = target[i] + 1; c <= 'z'; ++c) {
+        if (alph[c - 'a']) {
+          a.push_back(c);
+          alph[c - 'a']--;
+          flag = true;
+          break;
+        }
+      }
+
+      if (flag) {
+        return i;
+      }
+
+      if (i == 0) break;
+      alph[a.back() - 'a']++;
+      a.pop_back();
+    }
+
+    return nullopt;
+  }
+
+  string lexGreaterPermutation(string s, string target) {
+    for (char ch : s) {
+      alph[ch - 'a']++;
+    }
+
+    optional<int> index;
+    for (int i = 0; i < target.length(); ++i) {
+      if (alph[target[i] - 'a']) {
+        a.push_back(target[i]);
+        alph[target[i] - 'a']--;
+        continue;
+      }
+
+      index = backtrace(s, target, i);
+
+      if (!index) {
+        return (a == target) ? "" : a;
+      }
+      break;
+    }
+
+    if (!index) {
+      int lastPos = (int)target.length() - 1;
+      alph[a.back() - 'a']++;
+      a.pop_back();
+      index = backtrace(s, target, lastPos);
+      if (!index) return "";
+    }
+
+    for (int i = *index + 1; i < target.length(); ++i) {
+      for (int j = 0; j < 26; ++j) {
+        if (alph[j]) {
+          a.push_back(j + 'a');
+          alph[j]--;
+          break;
+        }
+      }
+    }
+
+    if (a == target) return "";
+
+    return a;
+  }
+};
+
+inline void run() {
+  runTests(
+      string(PROJECT_ROOT) + "/tests/3000-3999/3720",
+      "Lexicographically Smallest Permutation Greater Than Target",
+      [](istream& in) -> pair<string, string> {
+        string s = Parse::quotedString(in);
+        string target = Parse::quotedString(in);
+        return {s, target};
+      },
+      [](istream& in) -> vector<string> { return {Parse::quotedString(in)}; },
+      [](auto p) { return vector<string>{Solution().lexGreaterPermutation(p.first, p.second)}; });
+}
