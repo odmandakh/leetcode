dcf604966fa9ed7e8769d20151b4106c81d026fd
odmandakh <coding.achess@gmail.com>
2026-09-14 14:59:32 +0800

    punishment: easy

---
+++ problems/2000-2999/2824.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+
+using namespace std;
+
+class Solution {
+ public:
+  int countPairs(vector<int>& nums, int x) {
+    int cnt = 0, n = nums.size();
+
+    for (int i = 0; i < n; ++i) {
+      for (int j = i + 1; j < n; ++j) {
+        if (nums[i] + nums[j] < x) cnt++;
+      }
+    }
+
+    return cnt;
+  }
+};
