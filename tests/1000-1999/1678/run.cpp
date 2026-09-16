98cda56435c0b780b3525af65d5d445e3e555e2b
Dom <coding.achess@gmail.com>
2026-09-16 21:53:31 +0800

    feat: not know

--- tests/1000-1999/1678/run.cpp
+++ tests/1000-1999/1678/run.cpp
@@ -6,6 +6,12 @@ inline void run() {
       string(PROJECT_ROOT) + "/tests/1000-1999/1678",
       "Goal Parser Interpretation",
       Parse::quotedString,  // input: "abc" or 'abc' (quotes stripped)
+<<<<<<< Updated upstream
       [](istream& in) -> vector<string> { return {Parse::quotedLine(in)}; },
       [](auto s) { return vector<string>{Solution().interpret(s)}; });
+=======
+      Parse::strVec,         // output: single string (as a 1-element vector)
+      [](auto s) { return vector<string>{Solution().interpret(s)}; }
+  );
+>>>>>>> Stashed changes
 }
